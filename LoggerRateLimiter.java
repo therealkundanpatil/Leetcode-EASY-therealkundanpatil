49f36d52825e44d66946bd8e99272f5d0c34c3fb
therealkundanpatil <kp28021996@gmail.com>
2020-12-16 20:16:28 -0500

    -Logger Rate Limiter

---
+++ LoggerRateLimiter.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoggerRateLimiter {
+    Map< String, Integer > map;
+    /** Initialize your data structure here. */
+//    public Logger() {
+//        map = new HashMap<>();
+//    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(!map.containsKey(message)) {
+            map.put(message, timestamp);
+            return true;
+        } else {
+            if(timestamp - map.get(message) >= 10) {
+                map.put(message, timestamp);
+                return true;
+            }
+        }
+        return false;
+    }
+}
