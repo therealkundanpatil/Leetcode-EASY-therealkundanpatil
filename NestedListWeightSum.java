656e528299f36c232c25acc856cdff1f67194806
therealkundanpatil <kp28021996@gmail.com>
2021-01-26 10:02:31 -0500

    -Implemented Nested List weight sum

---
+++ NestedListWeightSum.java
@@ -0,0 +1,34 @@
+package easy;
+
+//import java.util.List;
+
+/**
+ * LeetCode Easy
+ * 
+ * Id:  Nested List Weight Sum
+ * 
+ * Time Complexity : O(n)
+ * 
+ * Space Complexity : O(1)
+ * 
+ * 
+ * @author Kundan
+ *
+ */
+public class NestedListWeightSum {
+//	public int depthSum(List<NestedInteger> nestedList) {
+//		return helper(nestedList, 1);
+//	}
+//
+//	private int helper(List<NestedInteger> nestedList, int depth) {
+//		int sum = 0;
+//		for (NestedInteger nest : nestedList) {
+//			if (nest.isInteger()) {
+//				sum += depth * nest.getInteger();
+//			} else {
+//				sum += helper(nest.getList(), depth + 1);
+//			}
+//		}
+//		return sum;
+//	}
+}
